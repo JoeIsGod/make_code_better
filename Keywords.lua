0ed4836941418d3a6dc2040a54e0c8209751924f
Joe <79620069+JoeIsGod@users.noreply.github.com>
2022-08-16 17:44:54 -0500

    Create Keywords.lua

---
+++ Keywords.lua
@@ -0,0 +1,9 @@
+
+local lookupify = loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeIsGod/make_code_better/main/lookupify.lua"))()
+
+return lookupify{
+    'and', 'break', 'do', 'else', 'elseif',
+    'end', 'false', 'for', 'function', 'goto', 'if',
+    'in', 'local', 'nil', 'not', 'or', 'repeat',
+    'return', 'then', 'true', 'until', 'while',
+};
