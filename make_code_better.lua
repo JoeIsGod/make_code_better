bad59300f2e76bdbac23082a40d43f4f7cbe6ed5
Joe <79620069+JoeIsGod@users.noreply.github.com>
2022-08-16 17:43:43 -0500

    Update make_code_better.lua

--- make_code_better.lua
+++ make_code_better.lua
@@ -1,4 +1,4 @@
-local LuaSyntaxToolset = loadstring(game:HttpGet(""))()
+local LuaSyntaxToolset = loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeIsGod/make_code_better/main/LuaSyntaxToolset.lua"))()
 return function(src)
 	return LuaSyntaxToolset.beautify(src, false, false, false)
 end
